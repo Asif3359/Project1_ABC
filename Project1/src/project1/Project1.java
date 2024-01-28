f41bee4af29849e6e51938f9ecd5e56f1f8af7c0
Asif3359 <asifahammed359@gmail.com>
2024-01-28 09:53:38 +0600

    start ABC Project for University

---
+++ Project1/src/project1/Project1.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package project1;
+
+/**
+ *
+ * @author HP
+ */
+public class Project1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        login1 login1Frame  = new login1();
+        login1Frame.setVisible(true);
+        login1Frame.pack();
+        login1Frame.setLocationRelativeTo(null);
+        
+    }
+    
+}
